ee22fd5bcafc190688bec076957c5a7136c185d2
Pavel Vashkel <jgugn1@gmail.com>
2017-01-19 11:52:17 +0300

    Initialize Paper. Create Stop model

---
+++ app/src/main/java/by/vshkl/translate/model/Stop.java
@@ -0,0 +1,71 @@
+package by.vshkl.translate.model;
+
+public class Stop {
+    private String url;
+    private String name;
+    private String direction;
+
+    public Stop() {
+    }
+
+    public Stop(String url, String name) {
+        this.url = url;
+        this.name = name;
+    }
+
+    public Stop(String url, String name, String direction) {
+        this.url = url;
+        this.name = name;
+        this.direction = direction;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Stop{");
+        sb.append("url='").append(url).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", direction='").append(direction).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Stop stop = (Stop) o;
+
+        return url.equals(stop.url);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return url.hashCode();
+    }
+}
